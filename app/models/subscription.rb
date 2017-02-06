bf5818dd9674f1d3514bdec8bc123ef7ecdeb846
pariente <parientethomas@gmail.com>
2017-02-06 15:47:22 +0100

    duplicate deleter for subscriptions placed in model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,4 +7,16 @@ class Subscription < ActiveRecord::Base
     self.save
   end
 
+  def self.dedupe
+    # find all models and group them on keys which should be common
+    grouped = all.group_by{|model| [model.user_id,model.source_id] }
+    grouped.values.each do |duplicates|
+      # the first one we want to keep right?
+      first_one = duplicates.shift # or pop for last one
+      # if there are any more left, they are duplicates
+      # so delete all of them
+      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
+
 end
