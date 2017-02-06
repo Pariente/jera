be7d3a94bc1d837ef63539446b0958c1854a90c7
pariente <parientethomas@gmail.com>
2017-02-06 15:41:58 +0100

    duplicate deleter for subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -14,6 +14,18 @@ class SubscriptionsController < ApplicationController
     Subscription.find(params[:id]).destroy
   end
 
+  def self.dedupe
+    # find all models and group them on keys which should be common
+    grouped = all.group_by{|model| [model.user_id,model.source_id] }
+    grouped.values.each do |duplicates|
+      # the first one we want to keep right?
+      first_one = duplicates.shift # or pop for last one
+      # if there are any more left, they are duplicates
+      # so delete all of them
+      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+    end
+  end
+
   def toggle_auto_harvest
     subscription = Subscription.find(params[:subscription_id])
     subscription.toggle_auto_harvest
