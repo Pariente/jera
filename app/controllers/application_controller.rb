3b892570c0e623b46b1465e2512523a82f6ea92e
pariente <parientethomas@gmail.com>
2017-02-07 16:52:22 +0100

    username issue solved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 
   # rescue_from ActiveRecord::RecordNotFound, with: :not_found 
   # rescue_from Exception, with: :not_found
