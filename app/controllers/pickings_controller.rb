ef3bfd88240e18036dc2ab31c59a69046dacc6d2
pariente <parientethomas@gmail.com>
2017-02-15 17:16:22 +0100

    proper garden and picking does not require reload page

--- app/controllers/pickings_controller.rb
+++ app/controllers/pickings_controller.rb
@@ -1,20 +1,27 @@
 class PickingsController < ApplicationController
   def new
-    # CHECKING IF PICKING EXISTS
     existing_picking = Picking.where(user_id: current_user.id, entry_id: params[:entry_id])
+    
+    # CHECKING IF PICKING EXISTS
     if existing_picking == []
+
       # IF IT DOES NOT, CREATE AND SAVE IT
       pick = Picking.create(user_id: current_user.id, entry_id: params[:entry_id])
       pick.save
-      # CLEARING CACHE, BUT NEED TO FIND OUT HOW TO ONLY EXPIRE THE CONCERNED FRAGMENT
-      e = Entry.find(params[:entry_id])
-      ActionController::Base.new.expire_fragment(%r{entry-#{e.id}/*})
+    else
+
+      # IF IT DOES, DESTROY IT
+      existing_picking.destroy_all
     end
+
+    # CLEARING THIS FRAGMENT FROM CACHE
+    e = Entry.find(params[:entry_id])
+    ActionController::Base.new.expire_fragment(%r{entry-#{e.id}/*})
   end
 
   def destroy
     Picking.find(params[:id]).destroy
-    # CLEARING CACHE, BUT NEED TO FIND OUT HOW TO ONLY EXPIRE THE CONCERNED FRAGMENT
+    # CLEARING THIS FRAGMENT FROM CACHE
     e = Entry.find(params[:entry_id])
     ActionController::Base.new.expire_fragment(%r{entry-#{e.id}/*})
   end
