7e4b82a61f0bf7f9f8586af6ef9b2062ebe4e5f3
pariente <parientethomas@gmail.com>
2017-02-26 18:21:59 +0100

    subscribe buttons dont require page reload

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,17 +1,15 @@
 class SubscriptionsController < ApplicationController
 
   def new
-    unless Subscription.where(source_id: params[:source_id], user_id: current_user.id) != []
+    if Subscription.where(source_id: params[:source_id], user_id: current_user.id) != []
+      Subscription.where(source_id: params[:source_id], user_id: current_user.id).destroy_all
+    else
       source = Source.find(params[:source_id])
       last_entry = source.entries.first
       new_entries = 0
       sub = Subscription.create(source_id: params[:source_id], user_id: current_user.id, last_entry_seen: last_entry.id, new_entries: new_entries)
       sub.save
     end
-  end
-
-  def destroy
-    Subscription.find(params[:id]).destroy
   end
 
 end
