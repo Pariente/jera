e7fedd793b8550f5759ab9abbb9b2a6799a11e9f
pariente <parientethomas@gmail.com>
2017-03-05 21:16:57 +0100

    fixed 500 error on sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  # The path used after sign up.
+  def after_sign_up_path_for(resource)
+    'trees/top'
+  end
+
   # GET /resource/sign_up
   # def new
   #   super
@@ -46,11 +51,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
   # end
 
   # The path used after sign up for inactive accounts.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_create :set_first_session_action
   has_many :subscriptions
   has_many :pickings
+  has_many :readings
   validates_uniqueness_of :username
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def set_first_session_action
+     self.last_session_last_action = Time.now
+  end
 end
