ed0e62b6c9b44a1db442c416fcca9d53d0c34886
pariente <parientethomas@gmail.com>
2017-04-18 16:48:48 +0200

    deleted duplicate migration

--- db/migrate/20170108184922_create_subscriptions.rb
+++ db/migrate/20170108184922_create_subscriptions.rb
@@ -1,4 +1,4 @@
-class CreateUsersSources < ActiveRecord::Migration
+class CreateSubscriptions < ActiveRecord::Migration
   def change
     create_table :subscriptions do |t|
       t.belongs_to :user, index: true
--- db/migrate/20170330112928_set_default_harvesed_to_false.rb
+++
@@ -1,5 +0,0 @@
-class SetDefaultHarvesedToFalse < ActiveRecord::Migration
-  def change
-    change_column_default(:entry_actions, :harvested, false)
-  end
-end
