d245ffae9eacfcb83eec3116aafc5a6a560ffbb9
pariente <parientethomas@gmail.com>
2017-04-18 16:59:53 +0200

    user method friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,17 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def friends
+    friendships_as_requester = Friendship.where(user_id: self.id, status: 1)
+    friendships_as_receiver = Friendship.where(friend_user_id: self.id, status: 1)
+    friends = []
+    friendships_as_requester.each do |f|
+      friends.push(User.find(f.friend_user_id))
+    end
+    friendships_as_receiver.each do |f|
+      friends.push(User.find(f.user_id))
+    end
+    return friends
+  end
 end
