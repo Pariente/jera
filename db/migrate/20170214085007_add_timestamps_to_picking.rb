f6c4bb668533a41c79d1626c1c556ca5920e088b
pariente <parientethomas@gmail.com>
2017-04-19 11:14:00 +0200

    removed old migration

--- db/migrate/20170214085007_add_timestamps_to_picking.rb
+++
@@ -1,9 +0,0 @@
-class AddTimestampsToPicking < ActiveRecord::Migration
-  def change
-    change_table :pickings do |t|
-      t.timestamps
-    end
-    Picking.update_all created_at: Time.now
-    Picking.update_all updated_at: Time.now
-  end
-end
