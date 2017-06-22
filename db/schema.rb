c9970c6d1a06c69a6d04f4bd6a9379dedc1799ad
pariente <parientethomas@gmail.com>
2017-06-22 09:28:59 +0200

    added last time checked values to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607143049) do
+ActiveRecord::Schema.define(version: 20170622072028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -107,20 +107,22 @@ ActiveRecord::Schema.define(version: 20170607143049) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",                                 null: false
-    t.string   "encrypted_password",     default: "",                                 null: false
+    t.string   "email",                      default: "",                                 null: false
+    t.string   "encrypted_password",         default: "",                                 null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,                                  null: false
+    t.integer  "sign_in_count",              default: 0,                                  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                                          null: false
-    t.datetime "updated_at",                                                          null: false
+    t.datetime "created_at",                                                              null: false
+    t.datetime "updated_at",                                                              null: false
     t.string   "username"
-    t.string   "picture",                default: "/images/default_user_picture.png"
+    t.string   "picture",                    default: "/images/default_user_picture.png"
+    t.datetime "last_time_checked_contacts"
+    t.datetime "last_time_checked_villages"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
