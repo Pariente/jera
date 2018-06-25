feade1233718fa6de7003f2c5ee95a13e0dae48e
pariente <parientethomas@gmail.com>
2018-06-25 10:08:42 +0200

    css iteration on empty messages

---
+++ db/migrate/20180407113739_remove_old_unharvested_entries.rb
@@ -0,0 +1,14 @@
+class RemoveOldUnharvestedEntries < ActiveRecord::Migration
+  def change
+    old_entries = Entry.where("created_at < ?", 1.year.ago)
+    unused_entries = []
+
+    old_entries.each do |e|
+      if e.entry_actions == []
+        unused_entries.push(e)
+      end
+    end
+
+    Entry.delete unused_entries.map { |u| u.id }
+  end
+end
